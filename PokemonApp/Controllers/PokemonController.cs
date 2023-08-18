3959224e78d319f086f183ebd99b2ab211760cd3
Roshan Sharma <roshan.sharma@capgemini.com>
2023-08-18 10:28:50 +0530

    consumed api in mvc

---
+++ PokemonApp/Controllers/PokemonController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonApp.Models;
+
+namespace PokemonApp.Controllers
+{
+    public class PokemonController : Controller
+    {
+        public IActionResult Index()
+        {
+            IEnumerable<PokemonViewModel> pokemons = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7065/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("pokemon");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<PokemonViewModel>>();
+                    readTask.Wait();
+
+                    pokemons = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    pokemons = Enumerable.Empty<PokemonViewModel>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(pokemons);
+        }
+    }
+}
---
+++ PokemonApp/Models/PokemonViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PokemonApp.Models
+{
+    public class PokemonViewModel
+    {
+        public int Id { get; set; }
+        public string PokemonName { get; set; }
+        public string PokemonType { get; set; }
+        public string PokemonPower { get; set; }
+        public string PokemonImage { get; set; }
+        public bool favourite { get; set; }
+
+        public int UserId { get; set; }
+        public UserModel UserModel { get; set; }
+    }
+}
